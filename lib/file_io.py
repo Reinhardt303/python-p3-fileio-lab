26fbed33c3ade7a3cb10dfc9afe9bf6fdf4b2980
Thomas <thomasrains01@gmail.com>
2025-01-22 08:07:39 -0800

    submits lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}', mode='w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}', mode='a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}', mode='r', encoding='utf-8') as file:
+        return file.read()
